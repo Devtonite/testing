04caa4280afb09c965316645b24aa6c4bc42ef84
devtonite <devtonite001@gmail.com>
2023-12-15 05:29:31 -0600

    Implement submit solution proof generation

--- src/Core.test.ts
+++ src/Core.test.ts
@@ -5,7 +5,10 @@ import { Field, Proof, Poseidon } from 'o1js';
 describe('SimpleProgram', () => {
 
   let rawUnitTest = "This is the unit test";
-  let tokenBountyValue = 1000;
+  let tokenBountyValue = 10;
+
+  let rawCodeSolution = "This is the code solution";
+  let tokenDepositValue = 1;
 
   let proof0: Proof<Field, void>;
   let proof1: Proof<Field, void>;
@@ -21,6 +24,14 @@ describe('SimpleProgram', () => {
     let unitTestHash = Poseidon.hash(unitTestFields);
 
     proof0 = await Core.buildBounty_Bf(tokenBounty, unitTestHash);
+  });
+
+  it('submit code solution', async () => {
+    let tokenDeposit = Field(tokenDepositValue);
+    let codeSolutionFields = stringToFields(rawCodeSolution);
+    let codeSolutionHash = Poseidon.hash(codeSolutionFields);
+
+    proof1 = await Core.commitSolution_Hf(tokenDeposit, codeSolutionHash, proof0);
   });
 
 });
--- src/Core.ts
+++ src/Core.ts
@@ -5,11 +5,22 @@ export const Core = ZkProgram({
   publicInput: Field,
 
   methods: {
+
     buildBounty_Bf: {
       privateInputs: [Field],
 
       method(tokenBounty: Field, unitTestHash: Field) {
-        tokenBounty.assertGreaterThan(Field(1));
+        tokenBounty.assertGreaterThanOrEqual(Field(10));
+      },
+    },
+
+    commitSolution_Hf: {
+      privateInputs: [Field, SelfProof],
+
+      method(tokenDeposit: Field, solutionHash: Field, bountyProof: SelfProof<Field, void>) {
+        // stake should be at least 10% of bounty 
+        let minimumStake = bountyProof.publicInput.div(10);
+        tokenDeposit.assertGreaterThanOrEqual(minimumStake);
       },
     },
 
